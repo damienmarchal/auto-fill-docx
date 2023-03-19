4c692ac8c1fd16ce7f33fe78420197db66426ec0
Damien Marchal <damien.marchal@univ-lille1.fr>
2023-03-19 16:06:40 +0100

    Add internal inline_shape.

--- auto-fill-docx.py
+++ auto-fill-docx.py
@@ -1,10 +1,10 @@
 import sys
 import os
-import csv                                        # Needed to process python 
+import csv                                        # Needed to process python
 from importlib.machinery import SourceFileLoader  # Needed to import python module from filename
 from subprocess import  Popen                     # Needed to call subproceess (libreoffice, pdftk)
 from collections import OrderedDict               # We are processing such a structure
-from docx import Document                         # Load, Modify and Save docx files. 
+from docx import Document                         # Load, Modify and Save docx files.
 
 
 def convert_to_pdf(input_docx, out_folder):
@@ -24,6 +24,10 @@ def substitute(template_file_path, variables, output_file_path):
     """Substitute the given variables in the template_file_path and save the resulting docx in output_file_path"""
     template_document = Document(template_file_path)
 
+    for i in template_document.inline_shapes:
+        for p  in i.part.document.paragraphs:
+            pass
+
     for variable_key, variable_value in variables.items():
         for paragraph in template_document.paragraphs:
             replace_text_in_paragraph(paragraph, variable_key, variable_value)
@@ -42,6 +46,7 @@ def replace_text_in_paragraph(paragraph, key, value):
         inline = paragraph.runs
         for item in inline:
             if key in item.text:
+                print("TEXT IS: ", item.text)
                 item.text = item.text.replace(key, value)
 
 if __name__ == '__main__':
@@ -51,32 +56,31 @@ if __name__ == '__main__':
     template_file_path = sys.argv[1]
     database_file_path = sys.argv[2]
     datacvt_file_path = sys.argv[3]
-    
-    input_data = open(database_file_path)
+
+    input_data = open(database_file_path, encoding="utf8")
     attendees = csv.DictReader(input_data, delimiter=',')
     print(attendees)
-        
-    # imports the module and evaluate it so we have a data producing system. 
+
+    # imports the module and evaluate it so we have a data producing system.
     data_to_token = SourceFileLoader(os.path.basename(sys.argv[3]),sys.argv[3]).load_module().patterns
 
-    
-    
+
+
     basename = os.path.splitext(os.path.basename(template_file_path))[0]
 
     attendees = list(attendees)
     attendees = sorted(attendees, key=lambda x: x['NOM'])
     out_list = []
-    for attendee in attendees:           
+    for attendee in attendees:
         rule = {}
         for token, get_data in data_to_token.items():
             rule[token] = get_data(attendee)
-        print(rule) 
-        
+        print(rule)
+
         out_file = basename+"-"+data_to_token["${ID}"](attendee)
         docx_file = "tmp/"+out_file+".docx"
-        substitute(template_file_path, rule, docx_file)        
-        convert_to_pdf(docx_file, "result") 
+        substitute(template_file_path, rule, docx_file)
+        convert_to_pdf(docx_file, "result")
         out_list.append("result/"+out_file+".pdf")
-    
+
     concat_all_pdf_in_one(out_list, basename+"-all-in-one.pdf")
-        
